a0c262d95c99c3972769940e17af64cc9a5bcfdf
Michael Wiley <mw3239@cloud.cs.columbia.edu>
2018-11-28 07:52:28 +0000

    added comments page functionality, fixed many minor bugs, such as search empty string, resgister with no username..., commented code, and added more to README.md

--- webserver1.0/README.md
+++ webserver1.0/README.md
@@ -1,26 +1,79 @@
+# Project 1: Japanese Learning Application
 
+Learning languages is difficult. Language courses have high attrition rate and people generally have trouble maintaining progress. In particular, Japanese features thousands of complex characters known as kanji that are difficult to write and remember the meaning of, taking learners many years to master. This application aims to help users read more web pages by displaying information about the kanji and words contained in some text.
+
+## Getting Started
+
+There are two ways to run the application, through the virtual instance or local machine.
+
+### To run from VM instance (if it is turned on)
+
+Have a working web browser such as firefox or chrome
+Enter in the following URL
+```
+104.196.39.47:8111
+```
+
+### To run locally
+
+Install python
 
 Install pip if needed
+```
+sudo apt-get install python-pip
+```
 
-        sudo apt-get install python-pip
-
-Install libraries
-
-        pip install click flask sqlalchemy
-
-
-Edit `server.py` to set your database URI
-
-        DATABASEURI = "<your database uri>"
-
+Install libraries	
+```
+pip install click flask sqlalchemy statistics os
+```
 
 Run it in the shell
-
-
-        python server.py
+```
+python server.py
+```
 
 Get help:
+```
+python server.py --help
+```
 
-        python server.py --help
+## Using the App
 
-      
+There are many functionalities and features implemented in the application
+
+### Home Page
+
+At the home page, enter any word, sentence, paragraph, or text containing Japanese in the search bar, and enter search get results.
+
+### About Page
+
+Detailed explanation on the purpose and functionalities of the project.
+
+### Analysis Page
+
+Gives detailed analysis and bits of interesting trivia about search requests.
+
+### User Account
+
+Maintains search history of logged in user.
+
+### Comments
+
+Forum pulling most recent comments from all users. Contains form box to submit new comment
+
+### Account Management (Login, Logout, Register)
+
+Allows users to register and account by username, and login and logout using said username.
+
+
+## Authors
+
+* **Michael Wiley** - github: [mw3239](https://github.com/mw3239) - email: [tw2686@columbia.edu](tw2686@columbia.edu)
+* **James Wong** - github: [tw2686](https://github.com/tw2686) - email: [tw2686@columbia.edu] (tw2686@columbia.edu) 
+
+## Acknowledgments
+
+* Eugene Wu - Instructor for COMS W4111 Introduction to Databases
+* Felipe Rocha - IA
+
--- webserver1.0/server.py
+++ webserver1.0/server.py
@@ -3,7 +3,9 @@
 
 """
 Columbia W4111 Intro to databases
-Example webserver
+Project 1: Real World Database Application
+- Michael Wiley, James Wong
+- Japanese Learning Application
 
 To run locally
 
@@ -20,6 +22,7 @@ from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response, url_for, session, abort, flash
 from statistics import mean
+from datetime import datetime
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -36,35 +39,35 @@ app = Flask(__name__, template_folder=tmpl_dir)
 
 # Use the DB credentials you received by e-mail
 
-
 DB_USER = "mw3239"
 DB_PASSWORD = "vfd41n26"
-#
+
 DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
-#
+
 DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/w4111"
-# #
-# # This line creates a database engine that knows how to connect to the URI above
-# #
+
+# This line creates a database engine that knows how to connect to the URI above
+
 engine = create_engine(DATABASEURI)
-#
-# # Here we create a test table and insert some values in it
+
+# Here we create a test table and insert some values in it
+
 engine.execute("""DROP TABLE IF EXISTS test;""")
 engine.execute("""CREATE TABLE IF NOT EXISTS test (
-id serial,
-name text
-);""")
+	id serial,
+	name text
+	);""")
 engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-#
+
 @app.before_request
 def before_request():
-#     """
-#     This function is run at the beginning of every web request
-#     (every time you enter an address in the web browser).
-#     We use it to setup a database connection that can be used throughout the request
-#
-#     The variable g is globally accessible
-#     """
+     """
+     This function is run at the beginning of every web request
+     (every time you enter an address in the web browser).
+     We use it to setup a database connection that can be used throughout the request
+
+     The variable g is globally accessible
+     """
      try:
          g.conn = engine.connect()
      except:
@@ -84,12 +87,14 @@ def teardown_request(exception):
         pass
 
 # Web Application
+# Home Page
 @app.route("/")
 def home():
     # DEBUG: this is debugging code to see what request looks like
     print request.args
     return render_template("index.html")
 
+# Global Variables
 maxkanji = ''
 maxcountk = 0
 maxword = ''
@@ -104,22 +109,26 @@ maxpos = ""
 username = ""
 url = ""
 
+# Search Functionality
 @app.route("/send", methods=['GET', 'POST'])
 def send():
     global maxkanji, maxcountk, maxword, maxcountw, readlvl, rarest_char, rarest_rank, diff_kanji, diff_strokes, maxpos, username, url
+    
+    # Check search request and its length
     if request.method == 'POST' and len(request.form['url']) != 0:
         url = request.form['url']	
 
-	# add search history
+	# If a user is logged in, add search request to search history
 	if len(username) != 0:
 		uidcursor = g.conn.execute("SELECT user_id FROM accounts WHERE username = '" + username + "'")
 		user_id = 0
 		for result in uidcursor:
 			user_id = result['user_id']
+		uidcursor.close()
 		insert = "INSERT INTO history(user_id, search) VALUES ("+str(user_id)+", '"+url.encode('utf-8')+"')"
 		g.conn.execute(insert)	
 
-	# getKanji
+	# Search for Kanji Characters
 	kanji_headers = ['Character', 'Strokes', 'Grade', 'jlpt', 'reading', 'translation', 'freq', 'rank']
 	kanji_headers = [s + ": " for s in kanji_headers]
 	kanji = []	
@@ -129,8 +138,7 @@ def send():
 		cursor = g.conn.execute("SELECT * FROM kanji WHERE character = '" + url[i] + "'")
 	       	character = []
 
-		#Define a list here that will contain all kanji already seen
-
+		# Define a list here that will contain all kanji already seen
 	        for result in cursor:
         		character.append(result['character'])
 			character.append(result['strokes'])
@@ -142,16 +150,17 @@ def send():
 			character.append(result['rank'])
 			encounteredk.append(result['character'])
 			enclvl.append(result['jlpt'])
-
 		cursor.close()
-		#Check if the number of number of times that the character is in the list is 1
-		#print(encountered))
 
+		# If no kanji characters were found return input
+		if len(character) == 0:
+        		return render_template('index.html', input=url)
+	
+		# Check if the number of number of times that the character is in the list is 1
 		if len(character) != 0 and encounteredk.count(character[0]) == 1:
 			kanji.append(character)
-
 	
-	# getDictionary
+	# Search for Words
 	dict_headers = ['Word', 'Reading', 'Definition', 'jlpt', 'Pos']
 	dict_headers = [s + ": " for s in dict_headers]
 
@@ -172,15 +181,13 @@ def send():
 				word.append(result['pos'])
 				encounteredw.append(result['word'])
 				encpos.append(result['pos'])
-
 			cursor2.close()
 			if encounteredw.count(word[0]) == 1:
 				dictionary.append(word)
 
-	# get Rarest kanji
+	# Find rarest kanji character
 	rarest_query = "SELECT character, rank FROM kanji WHERE character IN ('" +  "', '".join(encounteredk) + "') ORDER BY rank DESC LIMIT 1"
 	rarest = g.conn.execute(rarest_query)
-
 	for result in rarest:
 		rarest_char = result['character']
 		rarest_rank = result['rank']
@@ -203,44 +210,60 @@ def send():
 	maxpos = max(set(encpos),key=encpos.count)
 
         return render_template('index.html', input=url, kanji_headers=kanji_headers, kanji=kanji, dict_headers=dict_headers, dictionary=dictionary, zip=zip)
+
     else:
     	return render_template('index.html')
 
+# About Page
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
+# Analysis Page
 @app.route("/analysis")
 def analysis():
     global maxkanji, maxcountk, maxword, maxcountw, readlvl, rarest_char, rarest_rank, diff_kanji, diff_stroke, maxpos
+    
     return render_template('analysis.html', title='Analysis', maxkanji = maxkanji, maxcountk = maxcountk, maxword = maxword, maxcountw = maxcountw, readlvl = readlvl, rarest_char = rarest_char, rarest_rank = rarest_rank, diff_kanji = diff_kanji, diff_strokes = diff_strokes, maxpos = maxpos)
 
+# Accounts Page
 @app.route("/accounts")
 def accounts():
     global username, url
+    # Check if user is logged in
     if len(username) != 0:
+	# Get userid from username
 	uidcursor = g.conn.execute("SELECT user_id FROM accounts WHERE username = '" + username + "'")
 	uid = 0
 	for result in uidcursor:
 		uid = result['user_id']
+	uidcursor.close()
+	# Get search history
 	cursor = g.conn.execute("SELECT search FROM history WHERE user_id =" + str(uid))
 	history = []
 	for result in cursor:
 		history.append(result['search'])
+	cursor.close()
 	return render_template('accounts.html', title='Accounts', history=history, enumerate=enumerate)
+
     else:
     	return render_template('accounts.html', title='Accounts')
 
+# Register Page
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-    	username = request.form['username']
-   	try:
-		cursor = g.conn.execute("SELECT user_id FROM accounts ORDER BY user_id DESC LIMIT 1")
+	try:
+    		# Get max user id
+		username = request.form['username']
+		cursor = g.conn.execute("SELECT user_id FROM accounts ORDER BY user_id DESC LIMIT 1 OFFSET 2")
 		max_uid = 0
 		for result in cursor:
 			max_uid = result['user_id']
+		cursor.close()
 		max_uid += 1
+	
+		# Add new user to accounts
 		cmd = "INSERT INTO accounts(user_id, username) VALUES (" + str(max_uid) + ", '" + username + "')"
     		g.conn.execute(cmd)
 	except:
@@ -248,42 +271,104 @@ def register():
     else:
 	return render_template('register.html', title='Register')
 
+# Comments Page
+@app.route("/comments", methods=[ 'GET', 'POST' ])
+def comments():
+    global username
+    
+    # Look up all existing comments
+    cursor = g.conn.execute("SELECT cid, text, date, username FROM comments, accounts WHERE user_id = uid ORDER BY date DESC")
+    comments = []
+    dates = []
+    usernames = []
+    for result in cursor:
+	comments.append(result['text'])
+	dates.append(result['date'])
+	usernames.append(result['username'])
+    cursor.close()
+   
+    # Check if new comment was submitted 
+    if request.method == 'POST':
+	text = request.form['comment']
+	# Return if comment is empty
+	if len(text) == 0:
+		return render_template('comments.html', title='Comments', fail="You can't submit an empty comment.", comments=comments, dates=dates, usernames=usernames, zip=zip)
+	else:	
+		# Get max cid
+		cidcursor = g.conn.execute("SELECT cid FROM comments ORDER BY cid DESC LIMIT 1")
+		max_cid = 0
+		for result in cidcursor:
+			max_cid = result['cid']
+		cidcursor.close()
+		max_cid += 1
+	
+		# Anonymous uid
+		uid = 42069
+		# If a user logged in, replace uid
+		if len(username) != 0:
+			uidcursor = g.conn.execute("SELECT user_id FROM accounts WHERE username = '" + username + "'")
+			for result in uidcursor:
+				uid = result['user_id']
+			uidcursor.close()
+		# Get current date
+		dt = datetime.now().strftime('%Y-%m-%d')
+		# Insert new comment into comments table
+		cmd = "INSERT INTO comments(cid, text, date, uid) VALUES (" +str(max_cid)+ ", '" +text+ "', '" +dt+"', " +str(uid)+ ")"
+		g.conn.execute(cmd)	  	
+ 
+		# Look up updated comments
+    		cursor = g.conn.execute("SELECT cid, text, date, username FROM comments, accounts WHERE user_id = uid ORDER BY date DESC")
+    		comments = []
+    		dates = []
+    		usernames = []
+    		for result in cursor:
+			comments.append(result['text'])
+			dates.append(result['date'])
+			usernames.append(result['username'])
+		cursor.close()
+
+		return render_template('comments.html', title='Comments', comments=comments, dates=dates, usernames=usernames, zip=zip)
+
+    else:
+	return render_template('comments.html', title='Comments', comments=comments, dates=dates, usernames=usernames, zip=zip)	
+
+# Login Page
 @app.route("/login", methods=[ 'GET', 'POST' ])
 def login():
     global username
-    #if not session.get('logged_in'):
+    # Check if user is logged in
     if 'username' in session:
         return render_template('login.html', title='Login')
+    # Check if user requested log in
     elif request.method == 'POST':
         username = request.form['username']
-	cursor = g.conn.execute("SELECT DISTINCT username FROM accounts")
-	usernames = []
-	for result in cursor:
-		usernames.append(result['username'])
-	if username in usernames:
-        	session['username'] = username
-        	session['logged_in'] = True
-        	return render_template('login.html', title='Login')
+	if len(username) == 0:
+		return render_template('login.html', title='Login', fail="No username was specified")
 	else:
-		return render_template('login.html', title='Login', fail="Need to register username first")
+		# If username is not emtpy, look up all usernames
+		cursor = g.conn.execute("SELECT DISTINCT username FROM accounts")
+		usernames = []
+		for result in cursor:
+			usernames.append(result['username'])
+		cursor.close()
+		# If username is in database, authorize
+		if username in usernames:
+	        	session['username'] = username
+	        	session['logged_in'] = True
+	        	return render_template('login.html', title='Login')
+		else:
+			return render_template('login.html', title='Login', fail="Need to register username first")
     else:
         return render_template('login.html', title='Login')
 
+# Logout Page
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
     session.pop('username', None)
     return login()
 
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-    name = request.form['name']
-    print name
-    cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-    g.conn.execute(text(cmd), name1 = name, name2 = name);
-    return redirect('/')
-
+# Main function
 if __name__ == "__main__":
     import click
 
@@ -292,10 +377,12 @@ if __name__ == "__main__":
     @click.option('--threaded', is_flag=True)
     @click.argument('HOST', default='0.0.0.0')
     @click.argument('PORT', default=8111, type=int)
+    
     def run(debug, threaded, host, port):
         HOST, PORT = host, port
         print "running on %s:%d" % (HOST, PORT)
-        app.secret_key = os.urandom(12)
+        # Generate secret keys for sessions
+	app.secret_key = os.urandom(12)
         app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
     run()
